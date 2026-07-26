fd95c049afdcf00a68133ef6332fe71389a359fd
Lakshay1081 <lakshaypareek1805@gmail.com>
2026-07-26 22:43:16 +0530

    updated contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,13 +1,11 @@
 import ContactHero from "../components/contact/ContactHero";
 import ContactForm from "../components/contact/ContactForm";
-import ConsultationCTA from "../components/sections/ConsultationCTA";
 
 export default function Contact() {
   return (
     <>
       <ContactHero />
       <ContactForm />
-      <ConsultationCTA />
     </>
   );
 }
