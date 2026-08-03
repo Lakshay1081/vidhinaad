cdc8b29fb6a933e7664f74f76fd9252f897d9f14
Lakshay1081 <lakshaypareek1805@gmail.com>
2026-08-03 08:12:53 +0530

    deleted partners.jsx

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -5,7 +5,6 @@ import MainLayout from "../layouts/MainLayout";
 import Home from "../pages/Home";
 import About from "../pages/About";
 import Services from "../pages/Services";
-import Partners from "../pages/Partners";
 import Contact from "../pages/Contact";
 import Privacy from "../pages/Privacy";
 import Disclaimer from "../pages/Disclaimer";
@@ -19,7 +18,6 @@ function AppRoutes() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
-          <Route path="/partners" element={<Partners />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/privacy-policy" element={<Privacy />} />
           <Route path="/disclaimer" element={<Disclaimer />} />
